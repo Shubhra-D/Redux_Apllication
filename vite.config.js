eced1b9db8658102e3ec05c35040feed04dbe0c1
Shubhra Dwivedi <shubhradwivedi21@gmail.com>
2025-04-22 16:23:57 +0530

    Deployed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/Redux_Application",
   plugins: [react(), tsconfigPaths()],
 })
